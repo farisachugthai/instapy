ddf1bda3d9028229d13a373e1934de136b5b752f
farisachugthai <farischugthai@gmail.com>
2020-07-18 16:53:03 -0400

    Additional features script

---
+++ additional_features.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+==============================
+Additional Features in InstaPy
+==============================
+
+InstaPy is a sizable project that has a lot of
+`thoroughly documented <https://github.com/timgrossmann/InstaPy/blob/master/DOCUMENTATION.md>`_
+features. The good news is that if you’re feeling comfortable with the
+features you used above, then the rest should feel pretty similar. This
+section will outline some of the more useful features of InstaPy.
+
+"""
+from instapy_00 import username, password
+from instapy import InstaPy
+
+# Headless Browser
+# ================
+# This feature allows you to run your bot without the GUI of the browser. This
+# is super useful if you want to deploy your bot to a server where you may not
+# have or need the graphical interface. It’s also less CPU intensive, so it
+# improves performance. You can use it like so:
+session = InstaPy(username="Chugthai",
+                  password="pyQV7GcQVjBsU93mFy48", headless_browser=True)
+
+# Quota Supervisor
+# ================
+# You can’t scrape Instagram all day, every day. The service will quickly
+# notice that you’re running a bot and will ban some of its actions. That’s why
+# it’s a good idea to set quotas on some of your bot’s actions.
+session.set_quota_supervisor(
+    enabled=True, peak_comments_daily=240, peak_comments_hourly=21
+)
+
+# Using AI to Analyze Posts
+# =========================
+# Earlier you saw how to ignore posts that contain inappropriate words in their
+# descriptions. What if the description is good but the image itself is
+# inappropriate? You can integrate your InstaPy bot with ClarifAI, which offers
+# image and video recognition services:
+# Now your bot won’t like or comment on any image that ClarifAI considers NSFW.
+# You get 5,000 free API-calls per month.
+
+session.set_use_clarifai(enabled=True, api_key='<your_api_key>')
+session.clarifai_check_img_for(['nsfw'])
+
+# Relationship Bounds
+# ===================
+# It’s often a waste of time to interact with posts by people who have a lot of
+# followers. In such cases, it’s a good idea to set some relationship bounds so
+# that your bot doesn’t waste your precious computing resources:
+# With this, your bot won’t interact with posts by users who have more than
+# 8,500 followers.
+session.set_relationship_bounds(enabled=True, max_followers=8500)
